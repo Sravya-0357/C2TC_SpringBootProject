f9001be03dbae55bf0d15bfc077f3e692208f6be
DELL <DELL@SravyaPC2>
2025-08-08 22:28:41 +0530

    SpringWebProject

---
+++ SpringCoreWebProject/src/main/java/com/example/SpringCoreWebProject/SpringCoreWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCoreWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreWebProjectApplication.class, args);
+	}
+
+}
