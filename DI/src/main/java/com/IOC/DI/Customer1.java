5f9eaabcdb7aafde59371b3fc5eed4b95267b28b
DELL <DELL@SravyaPC2>
2025-08-09 07:33:24 +0530

    Dependency Injection and Autowired Annotation

--- DI/src/main/java/com/IOC/DI/Customer1.java
+++ DI/src/main/java/com/IOC/DI/Customer1.java
@@ -1,11 +1,18 @@
 package com.IOC.DI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer1 {
 	private int id;
 	private String cname;
+	
+	@Autowired
+	Customer2 cust2;
+	@Autowired
+	Customer3 cust3;
+	
 	public int getId() {
 		return id;
 	}
@@ -21,5 +28,7 @@ public class Customer1 {
 	
 	public void show() {
 		System.out.println("Customer 1 object");
+		cust2.show();
+		cust3.show();
 	}
 }
--- DI/src/main/java/com/IOC/DI/DiApplication.java
+++ DI/src/main/java/com/IOC/DI/DiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+
 @SpringBootApplication
 public class DiApplication {
 
@@ -12,13 +13,8 @@ public class DiApplication {
 		
 		Customer1 c1=var.getBean(Customer1.class);
 		
-		Customer2 c2=var.getBean(Customer2.class);
-		
-		Customer3 c3=var.getBean(Customer3.class);
 		
 		c1.show();
-		c2.show();
-		c3.show();
 		
 	}
 
